d27710f19e6a06f6e5394dd1713097835ffe1104
Sabbir Ahmed <sabbiirr@gmail.com>
2015-06-03 04:02:06 +0600

    Refactor clover config

--- Clover/Clover Config Intel/intel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -f intel.plist /Volumes/TEFI/EFI/CLOVER/config.plist
-echo "Copied config.plist for intel"
--- Clover/Clover Config NVIDIA & AMD/nvidia-amd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -f nvidia-amd.plist /Volumes/TEFI/EFI/CLOVER/config.plist
-echo "Copied config.plist for nvidia-amd"
