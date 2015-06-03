12dad413cbae86014c8bb8b7f49b934217d16411
Sabbir Ahmed <sabbiirr@gmail.com>
2015-06-03 15:18:29 +0600

    Fixed: would not installed if EFI parition already mounted

--- Clover/Clover UEFI/pre-install.sh
+++ Clover/Clover UEFI/pre-install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Formating EFI Partition ..."
+if [[ $(mount | awk '$3 == "/Volumes/EFI" {print $3}') != "" ]]
+then
+ 	echo "/Volumes/EFI is mounted"
+ 	echo "Unmounting.."
+ 	diskutil umount /Volumes/EFI
+fi
+
+echo "Formatting EFI"
 newfs_msdos -v EFI /dev/disk0s1
 
-echo "Mounting EFI Partition ..."
+echo "Mounting EFI"
 mkdir /Volumes/EFI
 mount_msdos /dev/disk0s1 /Volumes/EFI
